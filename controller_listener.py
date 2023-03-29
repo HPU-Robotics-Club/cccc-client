a504e7fcb5731b8cf065594a356a39e7fa7e0634
captaincrazybro <myeyesareonmizar@gmail.com>
2023-03-29 17:01:56 -0400

    Added support for the belt

--- controller_listener.py
+++ controller_listener.py
@@ -1,8 +1,9 @@
 from approxeng.input.selectbinder import ControllerResource
-from serial_communicator import SerialCommunicator, MotorCode
+from time import sleep
 import requests
 
-IP = "http://127.0.0.1:5000"
+hostname = "10.240.68.93"
+port = 3000
 
 def registerControllerListener():
     while True:
@@ -13,14 +14,23 @@ def registerControllerListener():
                 while joystick.connected:
                     x = joystick.rx
                     y = joystick.ry
+                    l1 = joystick["lt"]
+                    r1 = joystick["rt"]
+                    dup = joystick["dup"]
+                    ddown = joystick["ddown"]
 
-                    res = requests.post(IP, {
+                    IP = f'http://{hostname}:{port}'
+                    res = requests.post(IP, json={
                         "controller_x": x,
-                        "controller_y": y
+                        "controller_y": y,
+                        "controller_l1": l1,
+                        "controller_r1": r1,
+                        "controller_dup": dup,
+                        "controller_ddown": ddown
                     })
 
                     
-                    print(res.data())
+                    print(res.text)
 
         except IOError:
             print("Unable to find joystick!")
